da321b996c26fa56f5dc299a535d01360ed878ce
MinhND <dangminh@minhnd.com>
2017-02-21 11:07:40 +0700

    Implement User profile

--- user.go
+++ user.go
@@ -3,4 +3,62 @@ package fbbot
 type User struct {
 	ID          string `json:"id"`
 	PhoneNumber string `json:"phone_number,omitempty"`
+	isFetched bool
+	firstName	string `json:"first_name, omitempty"`
+	lastName	string `json:"last_name, omitempty"`
+	profilePic	string `json:"profile_pic, omitempty"`
+	locale		string `json:"locale, omitempty"`
+	timezone	int `json:"timezone, omitempty"`
+	gender		string `json:"gender, omitempty"`
+	isPaymentEnabled	bool `json:"is_payment_enabled, omitempty"` // Is the user eligible to receive messenger platform payment messages
+}
+
+func (u *User) FirstName() string {
+	if !u.isFetched {
+		bot.fetchUserData(u)
+	}
+	return u.firstName
+}
+
+func (u *User) LastName() string {
+	if !u.isFetched {
+		bot.fetchUserData(u)
+	}
+	return u.lastName
+}
+
+func (u *User) ProfilePic() string {
+	if !u.isFetched {
+		bot.fetchUserData(u)
+	}
+	return u.profilePic 
+}
+
+func (u *User) Locale() string {
+	if !u.isFetched {
+		bot.fetchUserData(u)
+	}
+	return u.locale
+}
+
+func (u *User) Timezone() int {
+	if !u.isFetched {
+		bot.fetchUserData(u)
+	}
+	return u.timezone
+}
+
+func (u *User) Gender() string {
+	if !u.isFetched {
+		bot.fetchUserData(u)
+	}
+	
+	return u.gender
+}
+
+func (u *User) IsPaymentEnabled() bool {
+	if !u.isFetched {
+		bot.fetchUserData(u)
+	}
+	return u.isPaymentEnabled
 }
