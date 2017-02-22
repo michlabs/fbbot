7513f0cad3ec7119211b4157535beff0c642e2ce
Hiep Ho Minh <hiephm@gmail.com>
2017-02-22 15:43:13 +0700

    Add NLP package with Wit Adapter

---
+++ nlp/client.go
@@ -0,0 +1,31 @@
+package nlp
+
+import (
+	"fmt"
+)
+
+var usedAdapter Client
+var adapters map[string]Client = make(map[string]Client)
+
+type Client interface {
+	Init(config map[string]string) error
+	Detect(msg string) (intent string, entities map[string][]string)
+}
+
+func Register(name string, client Client) {
+	adapters[name] = client
+}
+
+func Use(adapter string, config map[string]string)  error {
+	if _, ok := adapters[adapter]; !ok {
+		return fmt.Errorf("Adapter %s is not exist", adapter)
+	}
+	usedAdapter = adapters[adapter]
+	return usedAdapter.Init(config)
+}
+
+// Return detected intent and entities
+func Detect(msg string) (intent string, entities map[string][]string) {
+	return usedAdapter.Detect(msg)
+}
+
