23144b27a79d2e5948091ef66185911f98304944
MinhND <dangminh@minhnd.com>
2017-02-27 10:55:31 +0700

    Rename State to Step

--- dialog.go
+++ dialog.go
@@ -4,94 +4,94 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-// Event represents event triggered by state
+// Event represents event triggered in a step
 type Event string
 
 const ResetEvent Event = "reset"
 const NilEvent Event = ""
 
-type State interface {
+type Step interface {
 	Enter(*Bot, *Message) Event
 	Process(*Bot, *Message) Event
 	Leave(*Bot, *Message) Event
 }
 
-// BaseState is base struct for State
-type BaseState struct {
+// BaseStep is base struct for steps
+type BaseStep struct {
 	Name string
 }
 
-func (s BaseState) Enter(bot *Bot, msg *Message) (e Event)   { return e } // Do nothing
-func (s BaseState) Process(bot *Bot, msg *Message) (e Event) { return e } // Do nothing
-func (s BaseState) Leave(bot *Bot, msg *Message) (e Event)   { return e } // Do nothing
+func (s BaseStep) Enter(bot *Bot, msg *Message) (e Event)   { return e } // Do nothing
+func (s BaseStep) Process(bot *Bot, msg *Message) (e Event) { return e } // Do nothing
+func (s BaseStep) Leave(bot *Bot, msg *Message) (e Event)   { return e } // Do nothing
 
 type Dialog struct {
-	beginState State
-	endState   State
+	beginStep Step
+	endStep   Step
 
-	states   map[State]bool   // stores all states of this dialog
-	stateMap map[string]State // maps an user ID to his current state
-	transMap map[State]map[Event]State
+	steps   map[Step]bool   // stores all steps of this dialog
+	stepMap map[string]Step // maps an user ID to his current step
+	transMap map[Step]map[Event]Step
 }
 
 func NewDialog() *Dialog {
 	var d Dialog
-	d.states = make(map[State]bool)
-	d.stateMap = make(map[string]State)
-	d.transMap = make(map[State]map[Event]State)
+	d.steps = make(map[Step]bool)
+	d.stepMap = make(map[string]Step)
+	d.transMap = make(map[Step]map[Event]Step)
 
 	return &d
 }
 
-func (d *Dialog) AddStates(states ...State) {
-	for _, state := range states {
-		d.states[state] = true
+func (d *Dialog) AddSteps(steps ...Step) {
+	for _, step := range steps {
+		d.steps[step] = true
 	}
 }
 
-func (d *Dialog) SetBeginState(s State) {
-	d.beginState = s
+func (d *Dialog) SetBeginStep(s Step) {
+	d.beginStep = s
 }
 
-func (d *Dialog) SetEndState(s State) {
-	d.endState = s
+func (d *Dialog) SetEndStep(s Step) {
+	d.endStep = s
 }
 
-func (d *Dialog) AddTransition(src State, event Event, dst State) {
+func (d *Dialog) AddTransition(src Step, event Event, dst Step) {
 	_, exist := d.transMap[src]
 	if !exist {
-		d.transMap[src] = make(map[Event]State)
+		d.transMap[src] = make(map[Event]Step)
 	}
 	d.transMap[src][event] = dst
 }
 
-func (d *Dialog) AddGlobalTransition(event Event, dst State) {
-	for state := range d.states {
-		d.AddTransition(state, event, dst)
+func (d *Dialog) AddGlobalTransition(event Event, dst Step) {
+	for step := range d.steps {
+		d.AddTransition(step, event, dst)
 	}
 }
 
 func (d *Dialog) Handle(bot *Bot, msg *Message) {
-	if d.beginState == nil || d.endState == nil {
-		log.Fatal("BeginState and EndState are not set.")
+	if d.beginStep == nil || d.endStep == nil {
+		log.Fatal("BeginStep and EndStep are not set.")
 	}
 
 	var event Event
-	state := d.getState(msg.Sender.ID)
-	if state == nil || state == d.endState {
+	step := d.getStep(msg.Sender.ID)
+	if step == nil || step == d.endStep {
 		bot.STMemory.Delete(msg.Sender.ID)
-		d.setState(msg.Sender.ID, d.beginState)
-		state = d.getState(msg.Sender.ID)
-		event = state.Enter(bot, msg)
+		d.setStep(msg.Sender.ID, d.beginStep)
+		step = d.getStep(msg.Sender.ID)
+		event = step.Enter(bot, msg)
 	} else {
-		event = state.Process(bot, msg)
+		event = step.Process(bot, msg)
 	}
-	d.transition(bot, msg, state, event)
+	d.transition(bot, msg, step, event)
 }
 
-func (d *Dialog) transition(bot *Bot, msg *Message, src State, event Event) {
+func (d *Dialog) transition(bot *Bot, msg *Message, src Step, event Event) {
 	if event == ResetEvent {
-		d.resetState(msg.Sender.ID)
+		d.resetStep(msg.Sender.ID)
 		return
 	}
 
@@ -100,19 +100,19 @@ func (d *Dialog) transition(bot *Bot, msg *Message, src State, event Event) {
 		return
 	}
 	src.Leave(bot, msg)
-	d.setState(msg.Sender.ID, dst)
-	event = d.getState(msg.Sender.ID).Enter(bot, msg)
+	d.setStep(msg.Sender.ID, dst)
+	event = d.getStep(msg.Sender.ID).Enter(bot, msg)
 	d.transition(bot, msg, dst, event)
 }
 
-func (d *Dialog) setState(user_id string, state State) {
-	d.stateMap[user_id] = state
+func (d *Dialog) setStep(user_id string, step Step) {
+	d.stepMap[user_id] = step
 }
 
-func (d *Dialog) getState(user_id string) State {
-	return d.stateMap[user_id]
+func (d *Dialog) getStep(user_id string) Step {
+	return d.stepMap[user_id]
 }
 
-func (d *Dialog) resetState(user_id string) {
-	delete(d.stateMap, user_id)
+func (d *Dialog) resetStep(user_id string) {
+	delete(d.stepMap, user_id)
 }
