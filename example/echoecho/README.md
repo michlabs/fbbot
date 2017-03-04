ff23da144e239639e174746f3d6b13647c79dfdb
MinhND <dangminh@minhnd.com>
2017-03-04 18:11:39 +0700

    Update example

---
+++ example/echoecho/README.md
@@ -0,0 +1,11 @@
+## Echo Echo bot
+
+This bot do the easiest task in the world, just echo what it hears
+
+### Install
+```
+$ export GOPATH=$(pwd)
+$ export PATH=$PATH:$GOPATH/bin
+$ go get github.com/michlabs/fbbot/example/echoecho
+$ echoecho
+```
--- example/echoecho/main.go
+++ example/echoecho/main.go
@@ -9,12 +9,16 @@ const VERIFYTOKEN string = "your_secure_token"
 const PAGEACCESSTOKEN string = "your_beloved_page_access_token"
 
 func main() {
+	var e EchoEcho
+
 	bot := fbbot.New(PORT, VERIFYTOKEN, PAGEACCESSTOKEN)
-	bot.HandleMessage(MessageHandler)
+	bot.AddMessageHandler(e)
 	bot.Run()
 }
 
-func MessageHandler(bot *fbbot.Bot, msg *fbbot.Message) {
+type EchoEcho struct {}
+
+func (e EchoEcho) HandleMessage(bot *fbbot.Bot, msg *fbbot.Message) {
 	// Echo... echo...
 	m := fbbot.NewTextMessage(msg.Text)
 	bot.Send(msg.Sender, m)
